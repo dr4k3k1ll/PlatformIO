51b83dfc57554c041925d48d02dbb01ee998258f
dr4k3k1ll <michele.andrea.ammazzagatti@gmail.com>
2025-02-04 00:34:51 +0100

    Update Chiavi.h

--- Projects/TESI_SLAVE/src/Chiavi.h
+++ Projects/TESI_SLAVE/src/Chiavi.h
@@ -1,14 +1,14 @@
 #ifndef _CHIAVI_H_
 #define _CHIAVI_H_
 
-#define MY_WIFI_SSID "TIM-95654790"//STA1
-#define MY_WIFI_PASS "5HfmCdYa3hDLfkr2"//STA1
+#define MY_WIFI_SSID "your_id1"//STA1
+#define MY_WIFI_PASS "your_pass1"//STA1
 
-#define MY_WIFI_SSID_2 "iPhone di Michele Andrea" //STA2
-#define MY_WIFI_PASS_2 "dromedario"// STA2
+#define MY_WIFI_SSID_2 "your_id2" //STA2
+#define MY_WIFI_PASS_2 "your_pass2"// STA2
 
-#define MY_WIFI_SSID_3 "ESP_32_CAM_AP" //AP
-#define MY_WIFI_PASS_3 "Passw0rd" //AP
+#define MY_WIFI_SSID_3 "id_accesspoint" //AP
+#define MY_WIFI_PASS_3 "pass_accesspoint" //AP
 
 #endif
 
